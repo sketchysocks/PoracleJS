8ac6bedf282e4dc660c7286aaa6d7fd8a98fb297
sketchysocks <58572875+sketchysocks@users.noreply.github.com>
2020-07-07 11:05:17 -0400

    Update quest.js

--- src/controllers/quest.js
+++ src/controllers/quest.js
@@ -173,9 +173,13 @@ class Quest extends Controller {
 				const message = JSON.parse(mustache(view))
 
 				if (cares.ping) {
-					if (!message.content) message.content = cares.ping
-					if (message.content) message.content += cares.ping
-				}
+                                        if (!message.content) {
+                                                message.content = cares.ping;
+                                        } else {
+                                                message.content += cares.ping;
+                                        }
+                                }
+
 				const work = {
 					lat: data.latitude.toString().substring(0, 8),
 					lon: data.longitude.toString().substring(0, 8),
