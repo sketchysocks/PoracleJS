e557bdf69df1168d96c30d6269c04b4ae48452ba
sketchysocks <58572875+sketchysocks@users.noreply.github.com>
2020-08-17 11:46:20 -0400

    Update monster.js

--- src/controllers/monster.js
+++ src/controllers/monster.js
@@ -161,7 +161,7 @@ class Monster extends Controller {
 			data.emoji = e
 			data.emojiString = e.join('')
 
-			data.bestGreatLeagueRank = 9999
+			data.bestGreatLeagueRank = 4096
 			data.bestGreatLeagueRankCP = 0
 			if (data.pvp_rankings_great_league) {
 				for (const stats of data.pvp_rankings_great_league) {
@@ -174,7 +174,7 @@ class Monster extends Controller {
 				}
 			}
 
-			data.bestUltraLeagueRank = 9999
+			data.bestUltraLeagueRank = 4096
 			data.bestUltraLeagueRankCP = 0
 			if (data.pvp_rankings_ultra_league) {
 				for (const stats of data.pvp_rankings_ultra_league) {
